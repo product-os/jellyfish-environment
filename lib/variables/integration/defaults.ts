8c97f2ddc1978b9d5c57a15b5b287909fe8ebd1e
Josh Bowling <josh@balena.io>
2021-07-06 22:55:54 +0900

    Set default for Balena API OAuth base URL
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

---
+++ lib/variables/integration/defaults.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) Balena.io - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+const INTEGRATION_BALENA_API_OAUTH_BASE_URL = 'https://api.balena-cloud.com';
+
+export { INTEGRATION_BALENA_API_OAUTH_BASE_URL };
