9c9bd93894a026122723b7d20e967bd7c163451d
Carol Schulze <carol@ereski.org>
2022-02-23 14:35:31 -0300

    Upgrade redis options to v4
    Change-type: major
    Signed-off-by: Carol Schulze <carol@ereski.org>

--- lib/variables/redis/index.ts
+++ lib/variables/redis/index.ts
@@ -6,17 +6,21 @@ export interface Redis {
 	mock: boolean;
 	namespace: string;
 	password?: string;
-	host: string;
-	port: string;
+	socket: {
+		host: string;
+		port: string;
+	};
 }
 
 export function GetRedis(env: EnvironmentBuilder): Redis {
 	const options = {
 		mock: false,
 		namespace: env.getString('REDIS_NAMESPACE', defaults.REDIS_NAMESPACE),
-		host: env.getString('REDIS_HOST', defaults.REDIS_HOST),
-		port: env.getString('REDIS_PORT', defaults.REDIS_PORT),
 		password: env.getString('REDIS_PASSWORD', defaults.REDIS_PASSWORD),
+		socket: {
+			host: env.getString('REDIS_HOST', defaults.REDIS_HOST),
+			port: env.getString('REDIS_PORT', defaults.REDIS_PORT),
+		},
 	};
 	if (isEmpty(options.password)) {
 		Reflect.deleteProperty(options, 'password');
