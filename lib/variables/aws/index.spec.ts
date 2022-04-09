f4138c62491b4885133c7e11e3e0d24e1ad99235
Josh Bowling <josh@balena.io>
2022-04-09 01:09:23 +0000

    Update AWS S3 variables
    Add support for the AWS_S3_ENDPOINT variable.
    Production will not use this new variable as
    we let aws-sdk do this for us. Development
    will use this to store uploads in local
    s3-compatible minio service.
    
    Change-type: major
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/variables/aws/index.spec.ts
+++ lib/variables/aws/index.spec.ts
@@ -5,24 +5,32 @@ const variables = {
 	AWS_ACCESS_KEY_ID: 'foo',
 	AWS_SECRET_ACCESS_KEY: 'bar',
 	AWS_S3_BUCKET_NAME: 'baz',
+	AWS_S3_ENDPOINT: 'http://s3-endpoint.example.com',
 };
 
 describe('AWS', () => {
-	test('variables are set', () => {
-		const environment = getEnvironment(variables);
-		expect(environment.aws).toEqual({
-			accessKeyId: variables.AWS_ACCESS_KEY_ID,
-			secretAccessKey: variables.AWS_SECRET_ACCESS_KEY,
-			s3BucketName: variables.AWS_S3_BUCKET_NAME,
+	test('defaults are used for development', () => {
+		const environment = getEnvironment({
+			NODE_ENV: 'development',
+			...variables,
 		});
-	});
-
-	test('defaults are set', () => {
-		const environment = getEnvironment();
 		expect(environment.aws).toEqual({
 			accessKeyId: defaults.AWS_ACCESS_KEY_ID,
 			secretAccessKey: defaults.AWS_SECRET_ACCESS_KEY,
 			s3BucketName: defaults.AWS_S3_BUCKET_NAME,
+			s3Endpoint: variables.AWS_S3_ENDPOINT,
+		});
+	});
+
+	test('variables are set for production', () => {
+		const environment = getEnvironment({
+			NODE_ENV: 'production',
+			...variables,
+		});
+		expect(environment.aws).toEqual({
+			accessKeyId: variables.AWS_ACCESS_KEY_ID,
+			secretAccessKey: variables.AWS_SECRET_ACCESS_KEY,
+			s3BucketName: variables.AWS_S3_BUCKET_NAME,
 		});
 	});
 });
